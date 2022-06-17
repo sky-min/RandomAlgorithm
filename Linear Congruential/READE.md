d22d05a29eb5435f0f4a702768dd10c8a600048a
sky-min <ppt00003333@gmail.com>
2022-06-17 01:11:17 +0000

    LinearCongruential

---
+++ Linear Congruential/READE.md
@@ -0,0 +1,1 @@
+![image](./assets/LinearCongruential.svg)
---
+++ Linear Congruential/random.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdint.h>
+
+const int M = pow(2, 32);
+const int A = 22695477;
+const int C = 1;
+
+uint64_t rnum = time(NULL); //X0
+
+uint64_t randomMethod(){
+  rnum = (rnum * A + C) % M;
+  return rnum;
+}
+
+int main(){
+	int loop;
+	scanf("%d", &loop);
+	for(int i = 0; i < loop; i++){
+		printf("%d: %d\n", i, randomMethod());
+	}
+}
